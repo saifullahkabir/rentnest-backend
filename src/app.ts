72f98beef9a97faba1a2a6c92b608e152e1b316d
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-07-09 22:07:22 +0600

    implementing config

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import config from "./config";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
 app.use(
   cors({
-    origin: "",
+    origin: config.app_url,
     credentials: true,
   }),
 );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  app_url: process.env.APP_URL,
+  node_env: process.env.NODE_ENV,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+};
